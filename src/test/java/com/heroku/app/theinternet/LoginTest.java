fb3518f12e3baef2c00ecc810d182e912813d7a1
ocalm <ocal.mehmet@outlook.com.tr>
2022-08-09 16:37:01 +0300

    first commit

---
+++ src/test/java/com/heroku/app/theinternet/LoginTest.java
@@ -0,0 +1,21 @@
+package com.heroku.app.theinternet;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("https://the-internet.herokuapp.com/");
+
+        chromeDriver.manage().window().maximize();
+        System.out.println("Driver Baslatildi");
+        chromeDriver.quit();
+    }
+
+}
